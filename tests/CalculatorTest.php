1543fc1a1269fe15ec125e1eff0924f8b1759283
joaovictor-ip4y <joao.victor@ip4y.com.br>
2025-03-24 14:01:23 -0300

    Teste

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(4, $calculator->add(2, 2));
+    }
+}
